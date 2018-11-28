0275df33c0940a5e0de4439559c264fc51fbd5d8
ameen castellan <ameencastellan@ameens-Mac.local>
2018-11-28 16:02:15 +0500

    UI Files

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chandkareh
+//
+//  Created by ameen castellan on 11/28/18.
+//  Copyright © 2018 ameen castellan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  chandkareh
+//
+//  Created by ameen castellan on 11/28/18.
+//  Copyright © 2018 ameen castellan. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super .awakeFromNib()
+        layer.borderWidth = 4.0
+        layer.borderColor = UIColor.yellow.cgColor
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
